0ba10fd675c87982eeba5eac3554006a3a122c46
mskalecki <m.skalecki@heg.com.pl>
2019-11-21 16:37:02 +0100

    Updating project structure

--- 01_get_data.R
+++ 01_get_data.R
@@ -1,2 +1,32 @@
-## Test R file
+#' Getting data for prediction
+#' 
+#' @description
+#' * Reading forecasting data from csv file [data.csv] and temperature data from [temperature.csv]
+#' * Transforming and preparing data for model use.
+#' * Returning train and test sets
+#' 
+#' @params
+#' @param dateFrom - data range in whole data set starts from this date
+#' @param dateTo - data range in whole data set ends on this date
+#' @param dateToTrain - data range in train data set ends on this date; test set starts from this date
+#' 
+#' @output
+#' Returns train and test dataframes for forecasting models (see description)
 
+
+GetData <- function(dateFrom,
+                    dateTo,
+                    dateToTrain) {
+  
+  ## Read and merge data ----------------
+  
+  ## Read gas data
+  
+  
+  ## Read temperature data
+  
+  
+  ## Merge data
+
+  
+}
