54f515d166a3ce1fca5c57d445de2c56a614d6c5
mskalecki <m.skalecki@heg.com.pl>
2019-11-21 16:55:22 +0100

    Genetic algorithm description

--- 02_genetic_algorithm.R
+++ 02_genetic_algorithm.R
@@ -1,2 +1,29 @@
-## Test R file
+#' Genetic algorithm
+#' 
+#' @description
+#' Genetic algorith implementation
+#' 1. Population initialization ...
+#' 2. ...
+#' 
+#' 
+#' @param fun
+#' @param fun_train_set
+#' @param fun_test_set
+#' @param x_min
+#' @param x_max
+#' @param CEL
+#' @param popSize
+#' @param CR
+#' @param MR
+#' @param maxIter
+#' 
+#' 
+#' @return
+#' 
 
+
+geneticAlgorithm = function(fun, fun_train_set, fun_test_set, x_min, x_max, CEL, popSize, CR, MR, maxIter) {
+  
+  
+}
+  
