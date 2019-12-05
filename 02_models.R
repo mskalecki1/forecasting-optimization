563964d7e7ab7628a47609c589cebd163d4b9dd1
mskalecki <m.skalecki@heg.com.pl>
2019-12-05 12:35:06 +0100

    Added description

---
+++ 02_models.R
@@ -0,0 +1,18 @@
+#' Forecasting models
+#' 
+#' @description
+#' The code makes forecasting models using train and test tests from `01_get_data.R`.
+#' Finally there will be few models (eg. random forest, neural network, xgboost, svm, etc.)
+#' 
+#' @params
+#' 
+#' 
+#' @output
+#' Returns forecasting models
+
+
+
+GetModels <- function() {
+  
+}
+
