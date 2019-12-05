2cb4c790e7ef7e3136db3a139dc27df891b8ae0c
mskalecki <m.skalecki@heg.com.pl>
2019-12-05 12:40:00 +0100

    Final file with forecast

---
+++ forecast.R
@@ -0,0 +1,16 @@
+#' Time series forecasting
+#' 
+#' @description
+#' The code:
+#' * Gets data from `01_get_data.R`
+#' * Gets forecasting models from `02_models.R`
+#' * Optimizes models with genetic algorithm with `03_genetic_algorithm.R`
+#'   in order to minimize forecasting error
+#' * Makes predictions
+#' * Visualizes results
+#' 
+#' @params
+#' 
+#' 
+#' @output
+#' 
